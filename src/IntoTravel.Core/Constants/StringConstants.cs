00306fbb8ffc9d4226e85e2f350558e4fb248a54
Ryan <admin@bootbaron.com>
2017-09-29 14:39:50 -0700

    fix spelling

--- src/IntoTravel.Core/Constants/StringConstants.cs
+++ src/IntoTravel.Core/Constants/StringConstants.cs
@@ -2,7 +2,7 @@
 {
     public class StringConstants
     {
-        public const string BlobPreix = "https://intotravel.blob.core.windows.net";
+        public const string BlobPrefix = "https://intotravel.blob.core.windows.net";
         public const string CdnPrefix = "http://cdn.into.travel";
     }
 }
